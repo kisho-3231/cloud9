4728401cf8f9e4f08dcbf4cb5222914b3db05b89
EC2 Default User <ec2-user@ip-172-31-34-69.ap-south-1.compute.internal>
2024-12-12 06:07:04 +0000

    test2

---
+++ Untitled1.py
@@ -0,0 +1,13 @@
+# Python script to add two numbers
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the numbers
+result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {result}")
+
+
